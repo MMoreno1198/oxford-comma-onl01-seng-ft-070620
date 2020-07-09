ca94566e65d910c9fe6760899caa8c88bbf9fed7
Marcus Moreno <legendary.1198@gmail.com>
2020-07-09 23:04:58 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,7 @@
 def oxford_comma(array)
   
   if array.length == 2
+    
     return "#{array[0]} and #{array[1]}"
     
   elsif 2 < array.length
@@ -8,5 +9,7 @@ def oxford_comma(array)
     array[-1].insert(0, "and ")
     
   end
-  array.join(", ")    
+  
+  array.join(", ")  
+  
 end
